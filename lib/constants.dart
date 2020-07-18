60c2a12110e4ef0ce114a1342d0d7e90ac7e928b
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-07-19 00:01:28 +0700

    add small screen view & make headline constants

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+String kDicodingHeadline = 'Bangun Karirmu Sebagai\nDeveloper Profesional';
+String kDicodingSubHeadline =
+    'Visi dicoding adalah menjadi jaringan terbaik untuk developer di Indonesia. Dicoding memiliki dua misi utama. Pertama adalah membantu developer untuk menjadi entrepreneur yang mampu mengembangkan produk kelas dunia. Kedua adalah melahirkan talent digital sebanyak mungkin untuk industry IT di Indonesia.';
